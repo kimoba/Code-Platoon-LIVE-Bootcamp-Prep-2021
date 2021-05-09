69f03b759185102d8f92c2d0a091b1721fa71c55
Kim Oba <kimobachi@gmail.com>
2021-05-09 01:40:55 -0400

    lastFour of Social (Arrays)

---
+++ lastFour-of-Social-Arrays/README.md
@@ -0,0 +1,17 @@
+# Instructions
+
+Write a function that takes in a number that represents a social security number and return the last four digit as a string of the social security number. 
+
+**Example**
+
+```js
+lastFour(546800156) //-> 0156
+lastFour(657698357) //-> 8357
+lastFour(855790769) //-> 0769
+```
+
+Look into using the following built-in methods
+`.toString()`
+`.split()`
+`.slice()`
+`.join()`
---
+++ lastFour-of-Social-Arrays/index.js
@@ -0,0 +1,39 @@
+/*
+
+Write a function that takes in a number that represents a social security number and return the last four digit as a string of the social security number.
+ Look into using the following built-in methods .toString() .split() .slice() .join()
+
+*/
+
+
+// METHOD 1
+// toString + split + slice + join
+let lastFour = socSec => {
+  socSec = socSec.toString()
+  length = socSec.length
+
+  return socSec.split("").slice(length - 4).join("")
+}
+
+console.log(lastFour(546800156)) //-> 0156
+console.log(lastFour(657698357)) //-> 8357
+console.log(lastFour(855790769)) //-> 0769
+
+
+// METHOD 2
+// toString + new string + for loop + split + reverse + join
+let lastFour2 = (socSec) => {
+  socSec = socSec.toString()
+  let lastFourSSN = ""
+  // console.log(socSec.length - 1)
+  // console.log(socSec[0])
+  for (let i = (socSec.length - 1); i > (socSec.length - 5); i--) {
+    // console.log(socSec[i])
+    lastFourSSN = lastFourSSN + socSec[i]
+  }
+  return lastFourSSN.split("").reverse().join("")
+}
+
+// console.log(lastFour2(546800156)) //-> 0156
+// console.log(lastFour2(657698357)) //-> 8357
+// console.log(lastFour2(855790769)) //-> 0769
