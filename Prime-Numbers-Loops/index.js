a9294a15581a7e7e0c3862b275154b7816682e7c
Kim Oba <kimobachi@gmail.com>
2021-05-13 20:24:39 -0400

    for numbers like 21 and 49

--- Prime-Numbers-Loops/index.js
+++ Prime-Numbers-Loops/index.js
@@ -1,44 +1,14 @@
-/*
-A prime number is a positive integer with no divisors besides itself and 1 (it cannot be divided without a reminder by any number besides itself and 1).
-
-Write a function to check whether a given positive integer greater than 1 is prime or not.
-*/
-
-// CONDITIONAL TERNARY OPERATOR VER
-function isPrimeTernary(num) {
-  return ((num >= 1) && (num % 2 !== 0) ? true : false)
-}
-
-console.log(isPrimeTernary(3))  // => true
-console.log(isPrimeTernary(5))  // => true
-console.log(isPrimeTernary(10))  // => false (divided evenly by 2 and 5)
-
-
-
-// SWITCH STATEMENT
-let isPrime = num => {
-  switch(num >= 1) {
-    case num % 2 === 0:
-    return false
-    case num % 2 !== 0:
-    return true
+// IF STATEMENT
+function isPrime(num){
+  if (num > 0) {
+    for (let i = 2; i < num; i++) {
+      if (num % i === 0) {
+        return false
+      }
+    }
   }
+  return true
 }
-
 // console.log(isPrime(3))  // => true
 // console.log(isPrime(5))  // => true
 // console.log(isPrime(10))  // => false (divided evenly by 2 and 5)
-
-
-
-// IF ELSE STATEMENT
-function isPrime2(num) {
-  if ((num >= 1) && (num % 2 !== 0)) {
-    return true
-  }
-  return false
-}
-
-// console.log(isPrime2(3))  // => true
-// console.log(isPrime2(5))  // => true
-// console.log(isPrime2(10))  // => false (divided evenly by 2 and 5)
